9b930e1abffec7a55c93005068f7d48ad9cc98a7
Omar Sharaki <omarsharaki@gmail.com>
2020-07-05 19:56:30 +0200

    :feet: Made config file path relative

--- View.py
+++ View.py
@@ -141,7 +141,7 @@ def HandleClicks(clickPos):
                 gameTimerMS = controller.restart(gameTimerMS, livingCells, grid, buttons)
                 #print(livingCells)
             elif button.name == "readButton":
-                controller.readConfig("C:/Users/Owner/PyCharmProjects/Game of Life/configs/glider-gun.txt", grid, livingCells, blocksInCol//2, blocksInRow//2)
+                controller.readConfig("./configs/glider-gun.txt", grid, livingCells, blocksInCol//2, blocksInRow//2)
             return #if a button has been pressed, no need to keep searching
 
     for row in range(blocksInCol):
